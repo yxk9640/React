14afafe7cf4c800638596cc4942eb946c771a4dc
YXK <97624711+yxk9640@users.noreply.github.com>
2023-06-22 05:25:13 +0000

    Creating homepage file structure

---
+++ promptopia/app/layout.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+
+
+const metadata = {
+    title : "Promptopia",
+    description: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang = 'en'>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+                <Nav />
+                {children}
+            </main>
+        </body>
+
+
+    </html>
+  )
+}
+
+export default RootLayout;
